3dca439c62b213283574f553a634b722196a8c8a
ST10504417 <ST10504417@myemeris.edu.za>
2026-02-25 13:57:36 +0200

    Add files via upload

---
+++ ChairsActivity25Feb.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author AKST10504417
+ */
+public class ChairsActivity25Feb {
+    
+    public static void main(String[] args){
+        System.out.println("X                                                X"); 
+        System.out.println("X    ***                                    ***  X");
+        System.out.println("X   *   *                                  *   * X");
+        System.out.println("X    ***                                    ***  X");  
+        System.out.println("X     *                                      *   X");
+        System.out.println("X     ********       X X X X          ********   X");
+        System.out.println("X     *                 |                    *   X");
+        System.out.println("X     *******  X X X X X X X X X X     *******   X");
+        System.out.println("X X X X X X *  X                 X     * X X X X X");
+        System.out.println("X         X *  X      JAVA       X     * X       X");
+        System.out.println("X         X *  X                 X     * X       X");
+        System.out.println("X         X *  X                 X     * X       X");
+    }
+
+}
---
+++ ClassActivity2.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class ClassActivity2 {
+    
+    public static void main(String[] args) {
+        System.out.print("\t********\t\tXXXXXXXX");
+        System.out.print("\n    *\t\t  *\t    X\t\t  X");
+        System.out.print("\n    \b*\t S T O P   *\t    \bX\t   G O     X\t");
+        System.out.print("\n    *\t\t  *\t    X\t\t  X");
+        System.out.print("\n\t********\t\tXXXXXXXX");
+        System.out.print("\n\t   **   \t\t   XX   ");
+        System.out.print("\n\t   **   \t\t   XX   ");
+        System.out.print("\n\t   **   \t\t   XX   ");
+        System.out.print("\n\t   **   \t\t   XX   ");
+        System.out.print("\n\t  ****  \t\t  XXXX   ");
+        System.out.print("\n\t**********\t\tXXXXXXXXXX");
+    }
+    
+}
